efcc55e3af0606f825303632abdf9d2dd61c267e
Aurthur Musendame <aurthurmusendame@gmail.com>
2021-10-15 19:05:32 +0200

    updated settings

--- sample_tracking.sh
+++ sample_tracking.sh
@@ -1,5 +1,5 @@
 echo "Building Sample Tracking Central app for production ....";
-mvn clean install -DskipTests -Dspring.profiles.active=prod
+./mvnw clean install -DskipTests -Dspring.profiles.active=prod
 docker-compose build sampletracking
 docker-compose up -d sampletracking
 echo "Done building sample tracking :)";
